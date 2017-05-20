f657c1e2f41491cc1227769b4ddcfa01452e619b
Rob Garrison <wowmotty@gmail.com>
2017-05-20 11:47:25 -0500

    Add Turkish (tr) (#15)

---
+++ src/tr/tr.js
@@ -0,0 +1,76 @@
+{
+    "metadata": {
+        "alphabet": "Latn",
+        "continent": ["EU", "AS"],
+        "language": "Turkish",
+        "native": "Türkçe",
+        "source": [
+            "https://en.wikipedia.org/wiki/Turkish_language",
+            // The Turkish alphabet (Turkish: Türk Alfabesi) is an alphabet used
+            // for writing the Turkish language, derived from the Latin alphabet
+            "https://en.wikipedia.org/wiki/Turkish_alphabet"
+        ]
+    },
+    "data": {
+        "ç": {
+            "mapping": {
+                "base": "c"
+            }
+        },
+        "Ç": {
+            "mapping": {
+                "base": "C"
+            }
+        },
+        "ğ": {
+            "mapping": {
+                "base": "g"
+            }
+        },
+        "Ğ": {
+            "mapping": {
+                "base": "G"
+            }
+        },
+        "ı": {
+            "mapping": {
+                "base": "i"
+            }
+        },
+        "İ": {
+            "mapping": {
+                "base": "I"
+            }
+        },
+        "ö": {
+            "mapping": {
+                "base": "o"
+            }
+        },
+        "Ö": {
+            "mapping": {
+                "base": "O"
+            }
+        },
+        "ş": {
+            "mapping": {
+                "base": "s"
+            }
+        },
+        "Ş": {
+            "mapping": {
+                "base": "S"
+            }
+        },
+        "ü": {
+            "mapping": {
+                "base": "u"
+            }
+        },
+        "Ü": {
+            "mapping": {
+                "base": "U"
+            }
+        }
+    }
+}
